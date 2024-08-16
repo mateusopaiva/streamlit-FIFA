976289548be3fa486698defc0d6702638e9d7bda
Mateus Paiva <mateusopaiva@gmail.com>
2024-08-16 14:05:31 -0300

    Project

---
+++ 1_🏠_Home.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import webbrowser
+import pandas as pd
+import re
+from datetime import datetime 
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    df_data['Name'] = df_data['Name'].apply(lambda x: re.sub(r'^\d+\s+', '', x))
+
+    st.session_state["data"] = df_data
+
+st.markdown("# 🎮 FIFA 23 OFFICIAL DATASET! ⚽️")
+st.sidebar.markdown("Desenvolvido por [Mateus Paiva](https://www.linkedin.com/in/mateusopaiva/) e [Asimov Academy](https://asimov.academy)")
+
+
+btn = st.button("Acesse os dados no Kaggle")
+if btn:
+    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+
+st.markdown(
+    """
+    O conjunto de dados
+    de jogadores de futebol de 2017 a 2023 fornece informações 
+    abrangentes sobre jogadores de futebol profissionais.
+    O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos 
+    do jogador, características físicas, estatísticas de jogo, detalhes do contrato e 
+    afiliações de clubes. 
+    
+    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
+    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
+    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
+    desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e 
+    desenvolvimento do jogador ao longo do tempo.
+"""
+)
